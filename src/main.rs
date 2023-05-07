8b3ac1b7d8246b785e9961869d55568021567ea8
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-07 22:57:47 +0200

    match subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod args;
+mod build;
 
 fn main() {
-    let args = args::get_arguments();
+    let args: clap::ArgMatches = args::get_arguments();
+
+    match args.subcommand() {
+        Some(("build", _)) => {
+            build::build();
+        }
+
+        _ => {
+            unreachable!("Subcommand is required")
+        }
+    }
 }
